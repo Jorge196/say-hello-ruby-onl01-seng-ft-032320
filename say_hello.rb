651ceb407403af2286856d0775295a156c64d115
Jorge Ramirez <joramirez96@gmail.com>
2020-03-25 18:03:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
-# I call on the method, say_hello, and give it the string "Jorge"
-say_hello("Jorge")
-# The method prints this text to the screen:
-Hello Jorge!
+def say_hello(Jorge)
+  puts "Hello Jorge"
+end 
+
